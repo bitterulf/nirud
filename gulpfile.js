cbddaebd3c42501ade9e2621e49f1a44041754f2
bitterulf <bitterulf@users.noreply.github.com>
2018-10-02 16:45:46 +0200

    more gulp

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,11 @@ const gulp = require('gulp');
 const clean = require('gulp-clean');
 const Vinyl = require('vinyl');
 const fs = require('fs');
+const raster = require('gulp-raster');
+const rename = require('gulp-rename');
+const shell = require('gulp-shell');
+const dir = require('node-dir');
+const gm = require('gm');
 
 function generate(base, files) {
     const src = require('stream')
@@ -30,10 +35,27 @@ gulp.task('clean', function () {
     .pipe(clean());
 });
 
-gulp.task('default', ['clean'], function () {
+gulp.task('generate', ['clean'], function () {
     return generate('test', {
         '1.svg': fs.readFileSync('./base.svg').toString().split('#ffff00').join('#ffff00'),
         '2.svg': fs.readFileSync('./base.svg').toString().split('#ffff00').join('#00ff00')
     })
-    .pipe(gulp.dest('dest/'));
+    .pipe(raster({format: 'png', scale: 1}))
+    .pipe(rename({extname: '.png'}))
+    .pipe(gulp.dest('dest/'))
+    .pipe(shell([
+        'echo <%= file.path %>'
+    ]));
+});
+
+gulp.task('default', ['clean', 'generate'], function () {
+    const files = dir.files('./dest', {sync:true})
+
+    var g = gm();
+    files.forEach(function(file){
+        g.montage(file);
+    });
+    g.write('./dest/result.png', function(err) {
+        if(!err) console.log("Written montage image.");
+    });
 });
