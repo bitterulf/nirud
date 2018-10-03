cb73614575b1254b599e856e32c8e9a2b75fee28
Bitterulf <s_schlender@hotmail.com>
2018-10-03 19:25:15 +0200

    Improve

--- gulpfile.js
+++ gulpfile.js
@@ -8,6 +8,7 @@ const shell = require('gulp-shell');
 const dir = require('node-dir');
 const gm = require('gm');
 const jimp = require('gulp-jimp');
+const childProcess = require('child_process');
 
 function generate(base, files) {
     const src = require('stream')
@@ -38,40 +39,35 @@ gulp.task('clean', function () {
 
 gulp.task('generate', ['clean'], function () {
     return generate('test', {
-        '1.svg': fs.readFileSync('./base.svg').toString().split('#ffff00').join('#ffff00'),
-        '2.svg': fs.readFileSync('./base.svg').toString().split('#ffff00').join('#00ff00')
+        '1.svg': fs.readFileSync('./icon.svg').toString().split('#00ff00').join('#ff0000'),
+        '2.svg': fs.readFileSync('./icon.svg').toString().split('#00ff00').join('#00ff00'),
+        '3.svg': fs.readFileSync('./icon.svg').toString().split('#00ff00').join('#0000ff'),
+        '4.svg': fs.readFileSync('./icon.svg').toString().split('#00ff00').join('#00ffff'),
+        '5.svg': fs.readFileSync('./icon.svg').toString().split('#00ff00').join('#ff00ff'),
+        '6.svg': fs.readFileSync('./icon.svg').toString().split('#00ff00').join('#ffff00'),
     })
     .pipe(raster({format: 'png', scale: 1}))
     .pipe(rename({extname: '.png'}))
     .pipe(jimp({
         '-1': {
+            opacity: 0.2,
             type: 'png'
         },
         '-2': {
-            greyscale: true,
+            opacity: 0.6,
             type: 'png'
         },
         '-3': {
-            posterize: 2,
-            dither565: true,
-            background: '#ff0000',
+            opacity: 1,
             type: 'png'
         }
     }))
     .pipe(gulp.dest('dest/'))
     .pipe(shell([
-        'echo <%= file.path %>'
+        'gmic -i <%= file.path %> -gimp_cpencil 1.3,50,20,2,2,1,0 -gimp_graphic_novelfxl 1,2,6,5,20,0,0.62,14,0,1,0.5,0.78,1.92,0,0,0,1,1,1,0.5,0.8,1.28 -o <%= file.path %>'
     ]));
 });
 
 gulp.task('default', ['clean', 'generate'], function () {
-    const files = dir.files('./dest', {sync:true})
-
-    var g = gm();
-    files.forEach(function(file){
-        g.montage(file);
-    });
-    g.write('./dest/result.png', function(err) {
-        if(!err) console.log("Written montage image.");
-    });
+    childProcess.exec('gm montage -mode concatenate -tile 3x -background none ./dest/*.png ./dest/out.png');
 });
