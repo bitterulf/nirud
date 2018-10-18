6e2def55795e707a14ed27fe576f1fbc89955f93
bitterulf <bitterulf@users.noreply.github.com>
2018-10-18 10:18:31 +0200

    Add svg filtering

--- load.js
+++ load.js
@@ -5,8 +5,14 @@ const xml2js = require('xml2js');
 const xml = fs.readFileSync('./base.svg');
 
 parseString(xml, function (err, result) {
-    const oldStyle = _.get(result, 'svg.g[0].path[2].$.style');
-    _.set(result, 'svg.g[0].path[2].$.style', oldStyle.replace('fill-opacity:1;', 'fill-opacity:0;'));
+    // const oldStyle = _.get(result, 'svg.g[0].path[2].$.style');
+    // _.set(result, 'svg.g[0].path[2].$.style', oldStyle.replace('fill-opacity:1;', 'fill-opacity:0;'));
+
+
+    result.svg.g = result.svg.g.filter(function(g) {
+        return g.$['inkscape:label'] != 'Layer'
+    })
+
     const builder = new xml2js.Builder();
     const xml = builder.buildObject(result);
     fs.writeFileSync('./baseChanged.svg', xml);
